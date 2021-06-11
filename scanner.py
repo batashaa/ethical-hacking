4cf621913197e9dbb71257cfe36ef623fe9d796f
root <anup.vaze@valueleaf.com>
2021-06-11 10:48:30 +0530

    Added Ether request and combined ARP & ether packets

--- scanner.py
+++ scanner.py
@@ -2,7 +2,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp = scapy.ARP(pdst=ip) 
-    print(arp.summary())
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    request = broadcast/arp
+    print(request.summary())
+    arp.show()
+    broadcast.show()
+    request.show()
 
 scan('192.168.31.1/24')
 
