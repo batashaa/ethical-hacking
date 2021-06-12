c333b250ab27a9c1010bc165b4ed72c26b47a20c
root <anup.vaze@valueleaf.com>
2021-06-12 06:18:36 +0530

    Format the responses

--- scanner.py
+++ scanner.py
@@ -9,18 +9,16 @@ def scan(ip):
     request = broadcast/arp
     #Submit an coupled packet and receive responses
     #Fetch only answered
-    answered = scapy.srp(request, timeout=1)[0]
+    #Make the srp function non-verbose
+    answered = scapy.srp(request, timeout=1, verbose=False)[0]
 
+    print("IP\t\t\tMacAddr")
+    print("------------------------------------------")
     #Iterate over all the answered list of devices
     for element in answered:
         #Print IP and macaddress of the connected devices
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("-------------------------------------------------------------------")
-
-    #Print all answered endpoints
-    print(answered.summary())
-    #print(unanswered.summary())
-
+        print(element[1].psrc+"\t\t"+element[1].hwsrc)
+ 
+    
 scan('192.168.31.1/24')
 
